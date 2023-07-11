f957e1167af4242af553f8af1d89d1c1db2e21a3
sajjad-njr <sajjadrahman1020@gmail.com>
2023-07-12 01:54:38 +0600

    driver id track both present and absent

--- attend-count/atte.php
+++ attend-count/atte.php
@@ -2,16 +2,29 @@
 // Include the database connection
 require_once 'db.php';
 
+$car_model = $_POST["car_number"];
+$status = $_POST["status"];
+$amount = $_POST["amount"];
+$date = $_POST["date"];
+
+
 // Handle form submission to mark attendance
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
-   $count = "SELECT COUNT(*) FROM `driver_information` WHERE car_model=103 && date='2023-07-16'";
+    $count_query = "SELECT COUNT(*) FROM `driver_information` WHERE car_model = ? AND date = ?";
+    $stmt = $conn->prepare($count_query);
+    $stmt->bind_param("ss", $car_model, $date);
+    $stmt->execute();
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
+ 
+    echo "count = ".$count."\t klkl"."<br>";
+
+   echo "coutnt = ".$count."\t klkl"."<br>";
     if($count == 0)
     {
-        $car_model = $_POST["car_number"];
-        $status = $_POST["status"];
-        $amount = $_POST["amount"];
-        $date = $_POST["date"];
+
     
         $stmt = $conn->prepare("INSERT INTO driver_information (car_model, amount, status, date) VALUES (?, ?, ?, ?)");
         
@@ -26,32 +39,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     {
         echo "its assigne and find duplicate".$count;
     }
-
-    
-
-
-
 }
-
-// Display the attendance records for a given employee ID
-if (isset($_GET["employee_id"])) {
-    $car_number = $_GET["employee_id"];
-    $attendanceRecords = getAttendanceRecords($conn, $car_number);
-
-    echo "Attendance records for Employee ID: $car_number<br>";
-
-    if (count($attendanceRecords) > 0) {
-        foreach ($attendanceRecords as $record) {
-            $date = $record["date"];
-            $status = $record["status"];
-
-            echo "Date: $date - Status: $status<br>";
-        }
-    } else {
-        echo "No attendance records found for the specified employee ID.";
-    }
-}
-
+ 
 // Close the database connection
 $conn->close();
 ?>
